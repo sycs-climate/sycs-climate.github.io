578f36763789356ccbf70351331ee17eedd5b1f6
Neelu <neelu.rsa@gmail.com>
2019-04-03 20:19:37 +0100

    More Firebase stuff (trigger still commented out)

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,15 @@
-// // Set up Firebase Cloud Messaging
-// var messaging = firebase.messaging();
-//
-// messaging.usePublicVapidKey("BP8pKpFj6ppwFdAh4oQqs1YYDNI49RuCmA0yK7J_Kn2r2yCQcyp_7iQr4fhCO84A25g1aQR_hdV_3tPP39qzGA4");
-//
-//
+// // // Set up Firebase Cloud Messaging
+var messaging = firebase.messaging();
+messaging.usePublicVapidKey("BP8pKpFj6ppwFdAh4oQqs1YYDNI49RuCmA0yK7J_Kn2r2yCQcyp_7iQr4fhCO84A25g1aQR_hdV_3tPP39qzGA4");
+
 function Cookie() {
   // https://www.w3schools.com/js/js_cookies.asp
-
   this.set = function(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
   }
-
   this.get = function(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
@@ -36,62 +32,73 @@ function Cookie() {
 
 var cookie = new Cookie();
 
-//
-// function AskDialog(selector) {
-//   this.ask = function(message, yesFunction, noFunction) {
-//     $(selector + ' .message').html(message);
-//     $(selector + ' .yes.button').attr('onclick', yesFunction);
-//     $(selector + ' .no.button').attr('onclick', noFunction);
-//     $(selector).removeClass('hidden');
-//   }
-//   this.close = function() {
-//     $(selector + ' .message').html('');
-//     $(selector).addClass('hidden');
-//   }
-// }
-//
-// function Notification() {
-//   this.requestPermission = function(permission) {
-//     ask.close();
-//     if (permission) {
-//       cookie.set('notifications', 'yes', 150);
-//       notif.request();
-//     } else {
-//       cookie.set('notifications', 'no', 150);
-//     }
-//   }
-//   this.request = function() {
-//     if (cookie.get('notifications') == 'yes') {
-//       messaging.requestPermission().then(function() {
-//
-//       }).catch(function(err) {
-//         ask.ask('An error occurred while trying to enable push notifications. Would you like to try again?', 'notif.request()', '');
-//       });
-//     }
-//   }
-//   this.ask = function() {
-//     ask.ask('Would you like to receive push notifications with important news about upcoming strikes?', 'notif.requestPermission(true)', 'notif.requestPermission(false)');
-//   }
-// }
-//
 
-// var notif = new Notification();
-// var ask = new AskDialog('#askdialog');
-//
+function AskDialog(selector) {
+  this.ask = function(message, yesFunction, noFunction) {
+    $(selector + ' .message').html(message);
+    $(selector + ' .yes.button').attr('onclick', yesFunction);
+    $(selector + ' .no.button').attr('onclick', noFunction);
+    $(selector).removeClass('hidden');
+  }
+  this.close = function() {
+    $(selector + ' .message').html('');
+    $(selector).addClass('hidden');
+  }
+}
+var ask = new AskDialog('#askdialog');
+
+function requestNotifPermission(permission) {
+  ask.close();
+  if (permission) {
+    cookie.set('notifications', 'yes', 150);
+    requestNotif();
+  } else {
+    cookie.set('notifications', 'no', 150);
+  }
+}
+function requestNotif() {
+  if (cookie.get('notifications') == 'yes') {
+    messaging.requestPermission().then(function() {
+      getNotifToken();
+    }).catch(function(err) {
+      ask.ask('An error occurred while trying to enable push notifications. Would you like to try again?', 'requestNotif()', '');
+      console.log(err);
+    });
+  }
+}
+
+function askNotif() {
+  ask.ask('Would you like to receive push notifications with important news about upcoming strikes?', 'requestNotifPermission(true)', 'requestNotifPermission(false)');
+}
+
+function getNotifToken() {
+  messaging.getToken().then(function(currentToken) {
+    if (currentToken) {
+      sendNotifTokenToServer(currentToken);
+      notifSuccess();
+    } else {
+      askNotif();
+    }
+  }).catch(function(err) {
+    ask.ask('An error occurred with push notifications. Would you like to retry setup?', 'requestNotif()', '');
+  });
+}
+
+function notifSuccess() {
+  messaging.onTokenRefresh(function() {
+    notif.getToken();
+  });
+}
+
+function sendNotifTokenToServer(token) {
+  console.log(token);
+}
+
 // $(document).ready(function() {
-//   notif.ask();
-// });
-//
-//
-// messaging.getToken().then(function(currentToken) {
-//   if (currentToken) {
-//     sendTokenToServer(currentToken);
-//     updateUIForPushEnabled();
-//   } else {
-//     notif.ask();
-//     setTokenSentToServer(false);
+//   if (cookie.get('notifications') == '') {
+//     askNotif();
 //   }
-// }).catch(function(err) {
-//   showToken('Error retrieving Instance ID token. ', err);
-//   setTokenSentToServer(false);
+//   if (cookie.get('notifications') == 'yes') {
+//     getNotifToken();
+//   }
 // });
